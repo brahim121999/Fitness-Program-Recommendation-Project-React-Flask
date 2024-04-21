9768991826b5d055616b748439193245e85c0923
brahim121999 <bobo77726@gmail.com>
2024-04-21 20:47:17 +0200

    remove additional files

--- db.sql
+++
@@ -1,4 +0,0 @@
--- Pour la table "User"
-INSERT INTO "user" (name, email, password, weight, height, contact, address)
-VALUES ('dsfsdfdsf essfdsf', 'dsfsdd@dfsdf.com', 'passwsdfsdfdsford123', 70.5, 175.0, '123456789', '123 Main St'),
-       ('Jafsdfdsfdsfne Smith', 'fsdf@fdsdsfsdf.com', 'sfdfsdfsdfsd', 65.2, 160.0, '987654321', '456 Elm St');
